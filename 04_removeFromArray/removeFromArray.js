7f100beb21bf65db71239730b92cbf4b6ffb730e
Kangaa-Bit69 <61615273+Kangaa-Bit69@users.noreply.github.com>
2023-10-30 16:46:51 -0600

    finished exercises 3-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(myArray, ...arg) {
+ const newArray = [];
+ myArray.forEach((item) => {
+    if (!arg.includes(item)) {
+        newArray.push(item) ;
+    }
+ }); 
+ return newArray;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
