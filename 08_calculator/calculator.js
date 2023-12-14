4710b5d3d02000205243c7059d7ae47d39beb3d8
Kangaa-Bit69 <61615273+Kangaa-Bit69@users.noreply.github.com>
2023-12-14 11:48:22 -0700

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
 const add = function() {
 	
-};
-
+  console.log(arguments.length) 
+  let sum = 0;
+  if (arguments.length == 0) {
+    return arguments.length;
+   } else {
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    } return sum;
+   }
+}; 
 const subtract = function() {
-	
+  let sub = 0
+  for (i = 0; i < arguments.length; i++) {
+    sub = (arguments[i++]) - (arguments[i]);
+  } return sub;
 };
 
-const sum = function() {
-	
+// array.reduce uses an accumulator (newSum) as its first argument, the 0 after the addition is setting an initial value
+const sum = function(array) {
+	return array.reduce((newSum, current) => newSum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  } return product; 
 };
 
 // Do not edit below this line
